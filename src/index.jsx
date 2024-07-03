e73e5d0f38f98ba07825a33f6b79d570e4ee604e
happy436 <lelouch436@gmail.com>
2024-07-03 18:26:00 +0300

    basename

--- src/index.jsx
+++ src/index.jsx
@@ -3,21 +3,15 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-/* import { createStore } from "./store/createStore";
-import { Provider } from "react-redux"; */
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-//const store = createStore();
-
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		{/* <Provider store={store}> */}
-			<BrowserRouter>
+			<BrowserRouter basename="/testDashboard/">
 				<h1 className="hidden">test</h1>
 				<App />
 			</BrowserRouter>
 			<ToastContainer />
-		{/* </Provider> */}
 	</React.StrictMode>
 );
